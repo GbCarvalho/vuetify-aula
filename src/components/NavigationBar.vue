df40257b5a0a06ce01dc1baf2a7478cd5382a7ad
Gabriel Carvalho <gabriel.braga@prevision.com.br>
2021-11-04 13:10:23 -0300

    Used the navigation drawer in the class

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,4 +1,11 @@
 <template lang="pug">
+  v-navigation-drawer
+    h1 John Doe
+    v-divider
+    v-list
+      v-list-item(v-for="item in menuItens")
+        v-list-item-content
+          a(:href="item.link") {{item.title}}
 </template>
 
 <script>
